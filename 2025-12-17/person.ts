bf86eb05ac6fd326151f3674e3044c47e5d4d15a
asqwef <72994057+asqwef@users.noreply.github.com>
2025-12-17 19:47:15 +0900

    1, 2장 실습 코드

--- 2025-12-17/person.ts
+++ 2025-12-17/person.ts
@@ -10,4 +10,5 @@ export default class Person implements IPerson
     constructor(public name: string, public age: number) {}
 }
 
+
 export const makePerson = (name: string, age: number = makeRandomNumber()) => ({name, age})
